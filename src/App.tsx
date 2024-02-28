9a0e908f60fd61d76ce6bc2ca77ca5bb1c2a27ac
K.D.S. Akash <akashmusix01@gmail.com>
2024-02-29 03:27:40 +0530

    Button & alert example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <Button onClick={() => console.log("clicked")}>Click Here</Button>
+      {/* alert will be displayed only when the button is clicked */}
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          Submission Completed!
+        </Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>Submit</Button>
     </div>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -4,10 +4,22 @@ interface AlertProps {
   //children: is used to pass values as children to a component
   //ReactNode is used to pass html content inside the component
   children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert = ({ children }: AlertProps) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onClose }: AlertProps) => {
+  return (
+    <div className="alert alert-success alert-dismissible">
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
+      {children}
+    </div>
+  );
 };
 
 export default Alert;
